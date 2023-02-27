0f1579b2d1fd02914e0312adf3beba19b30502bc
Mateusz Janikowski <mateuszjanikowski02@gmail.com>
2023-02-27 21:30:05 +0100

    Enemy rework

---
+++ Assets/Scripts/KamikazeEnemy.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class KamikazeEnemy : MonoBehaviour
+{
+	[SerializeField] private float moveSpeed;
+	[SerializeField] private Vector2 targetDirection;
+	private Rigidbody2D _rigidbody;
+	
+	private void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	private void Update()
+	{
+		targetDirection = Vector2.ClampMagnitude(targetDirection, 1f);
+		_rigidbody.velocity = moveSpeed * targetDirection;
+	}
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
